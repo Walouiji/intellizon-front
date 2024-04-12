0a00aa4c21bb93f9dec7b02f6e16026d637964a2
chesiren <44122832+chesiren@users.noreply.github.com>
2024-04-12 15:16:47 +0200

    renommer SmartHome en IntelliZon

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'SmartHome' title`, () => {
+  it(`should have the 'IntelliZon' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SmartHome');
+    expect(app.title).toEqual('IntelliZon');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SmartHome');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, IntelliZon');
   });
 });
