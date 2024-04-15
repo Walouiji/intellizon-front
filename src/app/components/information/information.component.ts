1258fc857bf2a25d2f323de21c7396425cf86759
walouiji <alexandre.ward@ecole-hexagone.com>
2024-04-15 17:02:36 +0200

    add informations

--- src/app/components/information/information.component.ts
+++ src/app/components/information/information.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { HttpClientModule } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
-import {MatGridListModule} from '@angular/material/grid-list';
-import { API_KEY, API_BASE_URL } from '../../../environment/environment';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { API_BASE_URL } from '../../../environment/environment';
+import {MatIconModule} from '@angular/material/icon';
+import { ChartComponent } from '../chart/chart.component';
 
 const urlLastDataResource = API_BASE_URL + "/api/intellizon-front/getLatestData"
 const urlRangeDataResource = API_BASE_URL + "/api/intellizon-front/getDataRange"
 
-const headers = { 'Content-Type': 'application/json', 'Authorization': API_KEY };
-
 @Component({
 	selector: 'information',
 	standalone: true,
-	imports: [MatCardModule, MatDividerModule, HttpClientModule, DatePipe, MatGridListModule],
+	imports: [MatCardModule, MatDividerModule, HttpClientModule, DatePipe, MatGridListModule, MatIconModule, ChartComponent],
 	templateUrl: './information.component.html',
 	styleUrl: './information.component.scss'
 })
 export class InformationComponent {
-	// TO BE IMPLEMENTED
+
+	@Input() tempActuel!: number;
+	@Input() tempIdeal!: number;
+
 }
