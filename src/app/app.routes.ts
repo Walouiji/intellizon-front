c99313b8bc5d082a5848a063dd89f88b844ffd38
walouiji <alexandre.ward@ecole-hexagone.com>
2024-04-15 17:01:47 +0200

    add routes for debugging

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { ConfigComponent } from './components/pages/config/config.component';
+import { InformationComponent } from './components/information/information.component';
+import { ChartComponent } from './components/chart/chart.component';
 
 export const routes: Routes = [
     {
         'path': '',
         'component': HomeComponent,
+    },
+    {
+        'path': 'configuration',
+        'component': ConfigComponent
+    },
+    {
+        'path': 'info',
+        'component': InformationComponent
+    },
+    {
+        'path': 'chart',
+        'component': ChartComponent
     }
 ];
