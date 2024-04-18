a4afe07bf0a477368ea4b0e86b2f1dbec2b07d2c
chesiren <44122832+chesiren@users.noreply.github.com>
2024-04-18 09:55:55 +0200

    menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,14 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/pages/home/home.component';
 import { ConfigComponent } from './components/pages/config/config.component';
-import { CardComponent } from './components/information/card.component';
-import { ChartComponent } from './components/chart/chart.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 export const routes: Routes = [
     {
-        'path': '',
-        'component': HomeComponent,
+        path: '',
+        component: HomeComponent,
     },
     {
-        'path': 'configuration',
-        'component': ConfigComponent
-    },
-    {
-        'path': 'card',
-        'component': CardComponent
-    },
-    {
-        'path': 'chart',
-        'component': ChartComponent
+        path: 'config',
+        component: ConfigComponent,
     }
 ];
