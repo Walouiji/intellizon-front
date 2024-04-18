c39596384579e194d84d572b37b996e164dd260f
chesiren <44122832+chesiren@users.noreply.github.com>
2024-04-18 09:55:55 +0200

    menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ConfigComponent } from './components/config/config.component';
 
 export const routes: Routes = [
     {
-        'path': '',
-        'component': HomeComponent,
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'config',
+        component: ConfigComponent,
     }
 ];
