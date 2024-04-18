57e835e41a3ec770b69965cd34180ac93c8b7680
Alexandre <58483317+Walouiji@users.noreply.github.com>
2024-04-18 09:46:51 +0200

    Home page (#4)
    * ajout input card et import card dans home
    
    * add chart to card & change device
    
    * remove duplicated imports

--- src/app/components/information/card.component.ts
+++ src/app/components/information/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,6 +6,7 @@ import { DatePipe } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import {MatIconModule} from '@angular/material/icon';
 import { ChartComponent } from '../chart/chart.component';
+import { SensorService } from '../../services/sensor.service';
 
 @Component({
 	selector: 'card',
@@ -17,4 +18,9 @@ import { ChartComponent } from '../chart/chart.component';
 export class CardComponent {
 	@Input() tempActuel!: number;
 	@Input() tempIdeal!: number;
+
+	@Input() type!: string;
+    @Input() color!: string; // Au format "rrr, ggg, bbb"
+    @Input() data!: { time: Date; value: number; }[];
+
 }
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -7,6 +7,7 @@ import { SensorService } from '../../../services/sensor.service';
 import { CardComponent } from '../../information/card.component';
 import { ChartComponent } from "../../chart/chart.component";
 
+
 @Component({
     selector: 'home',
     standalone: true,
@@ -14,24 +15,22 @@ import { ChartComponent } from "../../chart/chart.component";
     styleUrl: './home.component.scss',
     imports: [CardComponent, ChartComponent, MatFormFieldModule, MatSelectModule, FormsModule]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit{
 
     public temperatureData!: { time: Date; value: number; }[];
     public humidityData!: { time: Date; value: number; }[];
     public lightData!: { time: Date; value: number; }[];
 
-    deviceList: any;
-    selectedDevice!: any;
+    deviceList: any = []
+    selectedDevice: any;
 
     constructor(private sensorService: SensorService) { }
 
-    ngOnInit() {
-        // this.getChartData(this.selectedDevice.deviceName, new Date('2024-04-16'), new Date('2024-04-17'));
-
-        this.sensorService.getDevices().subscribe(data => {
-            this.deviceList = data;
-            console.log(this.deviceList);
-          });
+    ngOnInit(): void {
+        this.sensorService
+            .getDevices().subscribe(devices => {
+                this.deviceList = devices;
+            });
     }
 
     /**
@@ -50,9 +49,8 @@ export class HomeComponent implements OnInit {
             });
     }
 
-    onSelect(device: any) {
-        console.log(device);
-        this.selectedDevice = device;
-        this.getChartData(this.selectedDevice.deviceName, new Date('2024-04-16'), new Date('2024-04-17'));
+    onSelect(event: any) {
+        this.selectedDevice = event;
+        this.getChartData(this.selectedDevice.deviceEui, new Date('2024-04-16'), new Date('2024-04-17'));
     }
 }
